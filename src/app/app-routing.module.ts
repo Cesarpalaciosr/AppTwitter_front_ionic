dfee5333d5798e0d9f960582467d424f6e41f27a
Cesar P <capr99@hotmail.com>
2023-11-07 00:04:11 -0400

    tweetbox counter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,6 +21,14 @@ const routes: Routes = [
   {
     path: 'twitster',
     loadChildren: () => import('./pages/twitster/twitster.module').then( m => m.TwitsterPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
   }
 ];
 @NgModule({
